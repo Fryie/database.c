0da6c7c33606296aca5be445e1563e032370dacd
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-06 23:19:46 +0200

    add insert_into skeleton and refactor

--- database.c
+++ database.c
@@ -22,6 +22,16 @@ typedef struct Table Table;
 Table *tables[MAX_TABLES];
 int num_tables = 0;
 
+int find_table(char *table_name) {
+  int index = -1;
+  for (int i = 0; i < num_tables; i++) {
+    if (tables[i] != NULL && strcmp(tables[i]->name, table_name) == 0) {
+      index = i;
+    }
+  }
+  return index;
+}
+
 void create_table(char *name) {
   Table *table = malloc(sizeof(Table));
   table->name = name;
@@ -30,35 +40,33 @@ void create_table(char *name) {
 }
 
 void drop_table(char *name) {
-  BOOL found = FALSE;
-  for (int i = 0; i < num_tables; i++) {
-    if (tables[i] != NULL && strcmp(tables[i]->name, name) == 0) {
-      found = TRUE;
-      free(tables[i]);
-      tables[i] = NULL;
-    }
+  int table_index = find_table(name);
+
+  if (table_index == -1) {
+    printf("Table not found: %s\n", name);
+    return;
   }
 
-  if (!found) {
-    printf("Table not found: %s\n", name);
-  }
+  free(tables[table_index]);
+  tables[table_index] = NULL;
 }
 
 void add_column(char *table_name, char *column_name) {
-  Table *table = NULL;
-  for (int i = 0; i < num_tables; i++) {
-    if (tables[i] != NULL && strcmp(tables[i]->name, table_name) == 0) {
-      table = tables[i];
-    }
-  }
+  int table_index = find_table(table_name);
 
-  if (table == NULL) {
+  if (table_index == -1) {
     printf("Table not found: %s\n", table_name);
     return;
   }
 
+  Table *table = tables[table_index];
   table->columns[table->num_columns++] = column_name;
 }
+
+void insert_into(char *table_name, char *values[], int num_values) {
+  return;
+}
+
 
 void list_columns(Table *table) {
   for (int i = 0; i < table->num_columns; i++) {
@@ -82,12 +90,18 @@ int main() {
   add_column("customers", "first_name");
   add_column("customers", "last_name");
   add_column("customers", "address");
+  char *values[] = {"John", "Doe", "3 Kensington Lane"};
+  insert_into("customers", values, 3);
   create_table("articles");
   add_column("articles", "name");
   add_column("articles", "price");
+  char *values2[] = {"MacBook Pro", "$1200"};
+  insert_into("articles", values2, 2);
   create_table("stores");
   add_column("stores", "city");
   add_column("stores", "size (in km^2)");
+  char *values3[] = {"San Francisco", "80"};
+  insert_into("stores", values3, 2);
   list_tables();
   drop_table("articles");
   list_tables();
