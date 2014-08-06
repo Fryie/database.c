852515692af38a119c7d727fad3357e910cf71e2
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-06 22:35:41 +0200

    add drop_table function

--- database.c
+++ database.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define FALSE 0
+#define TRUE 1
+#define BOOL short
 
 #define MAX_TABLES 10
 
@@ -18,9 +23,26 @@ void create_table(char *name) {
   tables[num_tables++] = table;
 }
 
+void drop_table(char *name) {
+  BOOL found = FALSE;
+  for (int i = 0; i < num_tables; i++) {
+    if (tables[i] != NULL && strcmp(tables[i]->name, name) == 0) {
+      found = TRUE;
+      free(tables[i]);
+      tables[i] = NULL;
+    }
+  }
+
+  if (!found) {
+    printf("Table not found: %s\n", name);
+  }
+}
+
 void list_tables() {
   for (int i = 0; i < num_tables; i++) {
-    printf("Table %d: %s\n", i, tables[i]->name);
+    if (tables[i] != NULL) {
+      printf("Table %d: %s\n", i, tables[i]->name);
+    }
   }
 }
 
@@ -28,6 +50,8 @@ int main() {
   create_table("customers");
   create_table("articles");
   create_table("buyers");
+  list_tables();
+  drop_table("articles");
   list_tables();
   /*
   first_name = select_from("first_name", "customers");
