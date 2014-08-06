bf06f96a1f1f914db30c063b8d024f154406f176
Pierpaolo Frasa <pierpaolo.frasa@twt.de>
2014-08-06 22:14:49 +0200

    refactor

--- database.c
+++ database.c
@@ -10,21 +10,25 @@ struct Table {
 typedef struct Table Table;
 
 Table *tables[MAX_TABLES];
-int i = 0;
+int num_tables = 0;
 
 void create_table(char *name) {
-  struct Table *table = malloc(sizeof(Table));
+  Table *table = malloc(sizeof(Table));
   table->name = name;
-  tables[i++] = table;
+  tables[num_tables++] = table;
+}
+
+void list_tables() {
+  for (int i = 0; i < num_tables; i++) {
+    printf("Table %d: %s\n", i, tables[i]->name);
+  }
 }
 
 int main() {
   create_table("customers");
   create_table("articles");
   create_table("buyers");
-  for (int j = 0; j < i; j++) {
-    printf("Table %d: %s\n", j, tables[j]->name);
-  }
+  list_tables();
   /*
   first_name = select_from("first_name", "customers");
   last_name = select_from("last_name", "customers");
